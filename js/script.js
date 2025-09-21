db3939c9c84d0199a579740ef922591f063365b3
Oluwamayowa Esan <esanoluwamayowa@gmail.com>
2025-09-21 19:26:52 +0000

    feat: more style updates

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
+const bioElement = document.getElementById("bio");
+
 const fetchBio = async () => {
-    const bio = document.getElementById("bio");
+    try {
+        const response = await fetch("https://api.github.com/users/battlesquid", {
+            headers: {
+                Accept: "application/vnd.github.v3+json"
+            }
+        });
 
-    const response = await fetch("https://api.github.com/users/battlesquid", {
-        headers: {
-            Accept: "application/vnd.github.v3+json"
+        if (response.ok) {
+            const data = await response.json();
+            return data.bio;
         }
-    });
-
-    if (response.ok) {
-        const data = await response.json();
-        bio.textContent = data.bio;
-    } else {
-        bio.textContent = "software engineer";
+    } catch (_) {
+        return "Software Engineer";
     }
 }
 
-fetchBio()
+const scrambleText = (text) => {
+    return [...text].map(() => String.fromCharCode(48 + Math.floor(Math.random() * 50))).join("");
+}
+
+const load = async () => {
+    const interval = setInterval(() => {
+        const text = scrambleText("00000000000");
+        bioElement.textContent = text;
+    }, 100);
+
+    const bio = await fetchBio();
+    setTimeout(() => {
+        clearInterval(interval);
+        bioElement.textContent = bio;
+    }, 1000);
+}
+
+load();
